4701748492037b5bf212316bd67e3b2633df4b43
Gábor Fási <m@maerlyn.eu>
2014-12-23 19:09:53 +0100

    generate doctrine entities

--- src/Maerlyn/TwitterBundle/Entity/Tweet.php
+++ src/Maerlyn/TwitterBundle/Entity/Tweet.php
@@ -76,4 +76,303 @@ class Tweet
      * @ORM\Column(type="boolean", options={ "default"=false })
      */
     protected $read = false;
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     * @return Tweet
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set coordinates
+     *
+     * @param string $coordinates
+     * @return Tweet
+     */
+    public function setCoordinates($coordinates)
+    {
+        $this->coordinates = $coordinates;
+
+        return $this;
+    }
+
+    /**
+     * Get coordinates
+     *
+     * @return string
+     */
+    public function getCoordinates()
+    {
+        return $this->coordinates;
+    }
+
+    /**
+     * Set created_at
+     *
+     * @param \DateTime $createdAt
+     * @return Tweet
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->created_at = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get created_at
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set favorited
+     *
+     * @param boolean $favorited
+     * @return Tweet
+     */
+    public function setFavorited($favorited)
+    {
+        $this->favorited = $favorited;
+
+        return $this;
+    }
+
+    /**
+     * Get favorited
+     *
+     * @return boolean
+     */
+    public function getFavorited()
+    {
+        return $this->favorited;
+    }
+
+    /**
+     * Set in_reply_to_screen_name
+     *
+     * @param string $inReplyToScreenName
+     * @return Tweet
+     */
+    public function setInReplyToScreenName($inReplyToScreenName)
+    {
+        $this->in_reply_to_screen_name = $inReplyToScreenName;
+
+        return $this;
+    }
+
+    /**
+     * Get in_reply_to_screen_name
+     *
+     * @return string
+     */
+    public function getInReplyToScreenName()
+    {
+        return $this->in_reply_to_screen_name;
+    }
+
+    /**
+     * Set in_reply_to_status_id
+     *
+     * @param integer $inReplyToStatusId
+     * @return Tweet
+     */
+    public function setInReplyToStatusId($inReplyToStatusId)
+    {
+        $this->in_reply_to_status_id = $inReplyToStatusId;
+
+        return $this;
+    }
+
+    /**
+     * Get in_reply_to_status_id
+     *
+     * @return integer
+     */
+    public function getInReplyToStatusId()
+    {
+        return $this->in_reply_to_status_id;
+    }
+
+    /**
+     * Set place
+     *
+     * @param string $place
+     * @return Tweet
+     */
+    public function setPlace($place)
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    /**
+     * Get place
+     *
+     * @return string
+     */
+    public function getPlace()
+    {
+        return $this->place;
+    }
+
+    /**
+     * Set retweeted
+     *
+     * @param boolean $retweeted
+     * @return Tweet
+     */
+    public function setRetweeted($retweeted)
+    {
+        $this->retweeted = $retweeted;
+
+        return $this;
+    }
+
+    /**
+     * Get retweeted
+     *
+     * @return boolean
+     */
+    public function getRetweeted()
+    {
+        return $this->retweeted;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     * @return Tweet
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set deleted_at
+     *
+     * @param \DateTime $deletedAt
+     * @return Tweet
+     */
+    public function setDeletedAt($deletedAt)
+    {
+        $this->deleted_at = $deletedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get deleted_at
+     *
+     * @return \DateTime
+     */
+    public function getDeletedAt()
+    {
+        return $this->deleted_at;
+    }
+
+    /**
+     * Set raw
+     *
+     * @param string $raw
+     * @return Tweet
+     */
+    public function setRaw($raw)
+    {
+        $this->raw = $raw;
+
+        return $this;
+    }
+
+    /**
+     * Get raw
+     *
+     * @return string
+     */
+    public function getRaw()
+    {
+        return $this->raw;
+    }
+
+    /**
+     * Set read
+     *
+     * @param boolean $read
+     * @return Tweet
+     */
+    public function setRead($read)
+    {
+        $this->read = $read;
+
+        return $this;
+    }
+
+    /**
+     * Get read
+     *
+     * @return boolean
+     */
+    public function getRead()
+    {
+        return $this->read;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Maerlyn\TwitterBundle\Entity\User $user
+     * @return Tweet
+     */
+    public function setUser(User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Maerlyn\TwitterBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
 }
--- src/Maerlyn/TwitterBundle/Entity/User.php
+++ src/Maerlyn/TwitterBundle/Entity/User.php
@@ -45,4 +45,165 @@ class User
      * @ORM\Column(type="string", nullable=true)
      */
     protected $url;
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     * @return User
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set screen_name
+     *
+     * @param string $screenName
+     * @return User
+     */
+    public function setScreenName($screenName)
+    {
+        $this->screen_name = $screenName;
+
+        return $this;
+    }
+
+    /**
+     * Get screen_name
+     *
+     * @return string
+     */
+    public function getScreenName()
+    {
+        return $this->screen_name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return User
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set profile_image_url
+     *
+     * @param string $profileImageUrl
+     * @return User
+     */
+    public function setProfileImageUrl($profileImageUrl)
+    {
+        $this->profile_image_url = $profileImageUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get profile_image_url
+     *
+     * @return string
+     */
+    public function getProfileImageUrl()
+    {
+        return $this->profile_image_url;
+    }
+
+    /**
+     * Set protected
+     *
+     * @param boolean $protected
+     * @return User
+     */
+    public function setProtected($protected)
+    {
+        $this->protected = $protected;
+
+        return $this;
+    }
+
+    /**
+     * Get protected
+     *
+     * @return boolean
+     */
+    public function getProtected()
+    {
+        return $this->protected;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return User
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
 }
